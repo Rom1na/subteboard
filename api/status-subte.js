cd2991d072c412f8e45d39786699f268f2ef70ef
Rom1na <romina.lc.perez@gmail.com>
2026-02-24 00:01:33 -0300

    Arreglando la ruta

--- api/status-subte.js
+++ api/status-subte.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res){
    const CLIENT_ID = process.env.CLIENT_ID;
    const CLIENT_SECRET = process.env.CLIENT_SECRET;
-   const urlOficial = `https://apitransporte.buenosaires.gob.ar/subtes/status?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+   const urlOficial = `https://apitransporte.buenosaires.gob.ar/subtes/forecastGTFS?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
 
    try{
         const response = await fetch(urlOficial);
